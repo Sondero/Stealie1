0b89ce590b8e6f7500a3a6eec9c6c3f15b8335c9
Sondero <sondre.toyotasen@gmail.com>
2018-05-22 11:27:08 +0200

    Made Changes additive

--- Source/Stealie1/Stealie1Character.cpp
+++ Source/Stealie1/Stealie1Character.cpp
@@ -12,6 +12,8 @@
 #include "Engine/World.h"
 #include "Components/SphereComponent.h"
 #include "Engine/Level.h"
+#include "Engine/SkeletalMesh.h"
+#include "Kismet/GameplayStatics.h"
 
 //////////////////////////////////////////////////////////////////////////
 // AStealie1Character
@@ -49,6 +51,9 @@ AStealie1Character::AStealie1Character()
 	CaughtSphere->AttachTo(RootComponent);
 	CaughtSphere->SetSphereRadius(80.0f);
 
+	//VisibleMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("VisibleMeshComponent"));
+	//VisibleMeshComponent->AttachTo(RootComponent);
+
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	//CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	//CameraBoom->SetupAttachment(RootComponent);
@@ -74,7 +79,6 @@ void AStealie1Character::SetupPlayerInputComponent(class UInputComponent* Player
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 	PlayerInputComponent->BindAction("Collect", IE_Pressed, this, &AStealie1Character::CollectPickups);
-	PlayerInputComponent->BindAction("Pause", IE_Pressed, this, &AStealie1Character::PauseGame);
 
 	PlayerInputComponent->BindAxis("MoveForward", this, &AStealie1Character::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &AStealie1Character::MoveRight);
@@ -101,19 +105,15 @@ void AStealie1Character::OnResetVR()
 	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
 }
 
-void AStealie1Character::PauseGame()
-{
-
-}
 
 void AStealie1Character::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
 {
-		Jump();
+	Jump();
 }
 
 void AStealie1Character::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
 {
-		StopJumping();
+	StopJumping();
 }
 
 void AStealie1Character::TurnAtRate(float Rate)
@@ -144,12 +144,12 @@ void AStealie1Character::MoveForward(float Value)
 
 void AStealie1Character::MoveRight(float Value)
 {
-	if ( (Controller != NULL) && (Value != 0.0f) )
+	if ((Controller != NULL) && (Value != 0.0f))
 	{
 		// find out which way is right
 		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
-	
+
 		// get right vector 
 		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 		// add movement in that direction
@@ -166,13 +166,55 @@ void AStealie1Character::CollectPickups()
 	for (int iCollected = 0; iCollected < CollectedActors.Num(); ++iCollected)
 	{
 		APickup* const TestPickup = Cast<APickup>(CollectedActors[iCollected]);
-		
+
 		if (TestPickup && !TestPickup->IsPendingKill() && TestPickup->IsActive())
 		{
 			TestPickup->WasCollected();
+
 			GetCharacterMovement()->MaxWalkSpeed = (GetCharacterMovement()->GetMaxSpeed()*PickupModifier);
+
 			GetCharacterMovement()->JumpZVelocity = (GetCharacterMovement()->JumpZVelocity*PickupJumpModifier);
-			TestPickup->SetActive(false);
+
+			FVector Actorscale = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorScale3D();
+			if ((Actorscale.X + PickupScaleModifier.X) < PickupScaleCap.X)
+			{
+				NewScaleX = (Actorscale.X) + PickupScaleModifier.X;
+
+			}
+			else
+			{
+				NewScaleX = PickupScaleCap.X;
+			}
+
+			if ((Actorscale.Y + PickupScaleModifier.Y) <= PickupScaleCap.Y)
+			{
+				NewScaleY = (Actorscale.Y) + PickupScaleModifier.Y;
+
+			}
+			else
+			{
+				NewScaleY = PickupScaleCap.Y;
+			}
+
+			if ((Actorscale.Z + PickupScaleModifier.Z) <= PickupScaleCap.Z)
+			{
+				NewScaleZ = (Actorscale.Z) + PickupScaleModifier.Z;
+
+			}
+			else
+			{
+				NewScaleZ = PickupScaleCap.Z;
+			}
+
+
+			FVector NewScale = { NewScaleX, NewScaleY, NewScaleZ };
+			GetWorld()->GetFirstPlayerController()->GetPawn()->SetActorScale3D(FVector(NewScale));
+
+			FString YourNewSize = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorScale3D().ToString();
+
+			UE_LOG(LogTemp, Warning, TEXT("Your new size is %s "), *YourNewSize)
+
+				TestPickup->SetActive(false);
 		}
 	}
 }
